b25833acf77e6f89a7835bbeb4c0e2480c546ec0
OtavioFSantos <otaviofs01@gmail.com>
2022-03-05 17:00:35 -0300

    typescript configured

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Index.module.css"
+
+export default function IndexPage() {
+  return (
+    <main>
+      <article>
+        <h1 className={styles.title}>Otávio é lindo</h1>
+      </article>
+    </main>
+  )
+}
