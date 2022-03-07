a82660bab0fafb70f54d53ac1ed6026f504369c1
OtavioFSantos <otaviofs01@gmail.com>
2022-03-07 10:57:29 -0300

    textarea and button added

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,18 @@ export default function IndexPage(props: Props) {
         <h1 className={styles.title}>Tweets: </h1>
 
         <article className={styles.centralize}>
+          <section className={styles.new_tweet}>
+            <textarea className={styles.textarea} rows={5} cols={35}></textarea>
+            <button className={styles.tweet_button}>Twittar</button>
+          </section>
+
           <section className={styles.timeline}>
             {props.tweets.map(tweet => (
               <div className={styles.tweet} key={tweet.id}>
                 <p>{tweet.content}</p>
 
                 <div className={styles.tweet_data}>
-                  <span>❤️ {tweet.likes}</span>
+                  <span>{tweet.likes} Likes</span>
                   <span>{new Date(tweet.createdAt).toLocaleDateString()}</span>
                 </div>
               </div>
