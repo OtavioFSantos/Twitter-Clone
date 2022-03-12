d85f7c7636c17be5d49f0e11d61e28d0b07deb6f
OtavioFSantos <otaviofs01@gmail.com>
2022-03-12 18:38:27 -0300

    code simplified & stretched on other files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,56 @@
-import { PrismaClient } from '@prisma/client'
-import styles from "../styles/Index.module.css"
-import { useForm } from 'react-hook-form'
+import { PrismaClient } from "@prisma/client";
+import { useQuery } from "react-query";
+import styles from "../styles/Index.module.css";
+import { Tweet } from "../lib/tweets/components/Tweet";
+import { CreateTweetForm } from "../lib/tweets/components/CreateTweetForm";
 
-type Props = Awaited<ReturnType<typeof getServerSideProps>>['props']
-
-const saveTweet = async (data) => {
-  await fetch("/api/tweet", {
-    method: "POST",
-    body: JSON.stringify(data)
-  })
-}
+type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
 
 export default function IndexPage(props: Props) {
-  const { register, handleSubmit } = useForm()
-
-  const onSubmit = async (data) => {
-    try {
-      await saveTweet(data)
-    } catch(err) {
-      console.log(err)
+  const query = useQuery(
+    "tweets",
+    () => fetch("/api/tweet").then((res) => res.json()),
+    {
+      initialData: props.tweets,
     }
-    
-  }
+  );
 
   return (
-      <main>
-        <h1 className={styles.title}>Tweets: </h1>
+    <main>
+      <h1 className={styles.title}>Tweets: </h1>
 
-        <article className={styles.centralize}>
-          <section className={styles.new_tweet}>
-            <form onSubmit={ handleSubmit(onSubmit) }>
-              <textarea className={styles.input_tweet} id="" cols={30} rows={3} {...register("content", { required: true })}></textarea>
-              <button className={styles.tweet_button}>Twittar</button>
-            </form>
-          </section>
+      <article className={styles.centralize}>
+        <section className={styles.new_tweet}>
+          <CreateTweetForm />
+        </section>
 
-          <section className={styles.timeline}>
-            {props.tweets.map(tweet => (
-              <div className={styles.tweet} key={tweet.id}>
-                <p>{tweet.content}</p>
-
-                <div className={styles.tweet_data}>
-                  <span>{tweet.likes} Likes</span>
-                  <span>{new Date(tweet.createdAt).toLocaleDateString()}</span>
-                </div>
-              </div>
-            ))}
-          </section>
-        </article>
-      </main>
-  )
+        <section className={styles.timeline}>
+          {query.data.map((tweet) => (
+            <Tweet key={tweet.id} tweet={tweet} />
+          ))}
+        </section>
+      </article>
+    </main>
+  );
 }
 
 export async function getServerSideProps() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
   const tweets = await prisma.tweet.findMany({
     select: {
       id: true,
       content: true,
       likes: true,
-      createdAt: true
-    }
-  })
+      createdAt: true,
+    },
+  });
 
   return {
     props: {
-      tweets: tweets.map(tweet => ({ ...tweet, createdAt: tweet.createdAt.toISOString() }))
-    }
-  }
+      tweets: tweets.map((tweet) => ({
+        ...tweet,
+        createdAt: tweet.createdAt.toISOString(),
+      })),
+    },
+  };
 }
