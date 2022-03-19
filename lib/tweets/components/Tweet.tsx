7f144c56dc6df7b9efc260b0b3ccdc8ed2346f58
OtavioFSantos <otaviofs01@gmail.com>
2022-03-19 18:40:40 -0300

    link to user page added

--- lib/tweets/components/Tweet.tsx
+++ lib/tweets/components/Tweet.tsx
@@ -1,6 +1,7 @@
 import styles from "./Tweet.module.css";
 import type { Tweet as TweetType } from "@prisma/client";
 import { useMutation, useQueryClient } from "react-query";
+import Link from "next/link";
 
 type Props = {
   tweet: {
@@ -9,46 +10,48 @@ type Props = {
     likes: TweetType["likes"];
     createdAt: string;
   };
+  userName: string;
+  userId: string;
 };
 
-const handleLike = (data) => 
+const handleLike = (data) =>
   fetch("/api/likes", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", //tenho duvida se é assim
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
+  }).then((res) => res.json());
 
-
-export function Tweet({ tweet }: Props) {
+export function Tweet({ tweet, userId, userName }: Props) {
   const queryClient = useQueryClient();
   const mutation = useMutation("update-likes", handleLike, {
     onSuccess: () => {
       queryClient.invalidateQueries(["tweets"]);
     },
-  })
+  });
 
   const onSubmit = (ev) => {
     ev.preventDefault();
     mutation.mutate(tweet);
-  }
+  };
 
   return (
     <div className={styles.tweet} key={tweet.id}>
       <div className={styles.person}>
-        <span className={styles.name}>Anonymous</span>
-        <span className={styles.username}>@anonymous · </span>
-        <span>{new Date(tweet.createdAt).toLocaleDateString()}</span>
+        <Link href={`/page/${userId}`}>
+          <a className={styles.name}>{userName}</a>
+        </Link>
+        <span> - {new Date(tweet.createdAt).toLocaleDateString()}</span>
       </div>
 
       <p className={styles.content}>{tweet.content}</p>
 
       <div className={styles.tweet_data}>
-        <button className={styles.likeButton} onClick={
-          onSubmit/*() => mutation.mutate(tweet)*/
-          }></button>
-        <span>{tweet.likes} {tweet.likes > 0 ? "Likes" : "Like"}</span>
+        <button className={styles.likeButton} onClick={onSubmit}></button>
+        <span>
+          {tweet.likes} {tweet.likes > 0 ? "Likes" : "Like"}
+        </span>
       </div>
     </div>
   );
