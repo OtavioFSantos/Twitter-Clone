eb16f0396de4a86013d8d156ca0bab3050113ee5
OtavioFSantos <otaviofs01@gmail.com>
2022-03-19 18:44:42 -0300

    only authenticated can tweet

--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -1,23 +1,44 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 import { PrismaClient } from "@prisma/client";
 import { Tweet } from "../../lib/tweets/Tweet";
 import { TweetsService } from "../../lib/tweets/services/TweetsService";
+import { UserService } from "../../lib/users/services/UserService";
 
 const prisma = new PrismaClient();
 const tweetsService = new TweetsService(prisma);
+const userService = new UserService(prisma);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const tweet = new Tweet(req.body.content);
-    const newTweet = await tweetsService.create(tweet);
-    return res.status(201).json(newTweet);
+    // só deve fazer tweet quem tiver autenticado
+    const session = await getSession({ req });
+
+    if (!!session && !!session.user) {
+      session.user.email;
+      const user = await userService.findByEmail(session.user.email);
+      if (!user) {
+        return res.status(401).json({
+          message: "Unauthorized",
+        });
+      }
+
+      const tweet = new Tweet(req.body.content);
+
+      const newTweet = await tweetsService.create(tweet, user.id);
+      return res.status(201).json(newTweet);
+    }
+
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
   }
 
   if (req.method === "GET") {
-    const tweets = await tweetsService.list()  //acredito estar correto
+    const tweets = await tweetsService.list();
     return res.status(200).json(tweets);
   }
 
