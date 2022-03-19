ae29964681c7516dd94ecf741018f8a0c0e1fed0
OtavioFSantos <otaviofs01@gmail.com>
2022-03-19 18:37:13 -0300

    user add on findMany

--- lib/tweets/services/TweetsService.ts
+++ lib/tweets/services/TweetsService.ts
@@ -4,13 +4,14 @@ import { Tweet } from "../Tweet";
 export class TweetsService {
   constructor(private readonly db: PrismaClient) {}
 
-  async create(tweet: Tweet) {
-    if(tweet.content.length > 200){
-      throw new Error('Exceeded max length');
+  async create(tweet: Tweet, userId: string) {
+    if (tweet.content.length > 200) {
+      throw new Error("Exceeded max length");
     }
 
     return this.db.tweet.create({
       data: {
+        userId,
         content: tweet.content,
         likes: tweet.likes ?? 0,
         createdAt: tweet.createdAt,
@@ -18,13 +19,14 @@ export class TweetsService {
     });
   }
 
-  async list(){
+  async list() {
     return this.db.tweet.findMany({
       select: {
         id: true,
         content: true,
         likes: true,
         createdAt: true,
+        user: true,
       },
       orderBy: {
         createdAt: "desc",
@@ -32,7 +34,7 @@ export class TweetsService {
     });
   }
 
-  async updateLike(tweet: Tweet) {  //parece correto
+  async updateLike(tweet: Tweet) {
     return this.db.tweet.update({
       where: {
         id: tweet.content["id"],
