9378815e34b908ddc6fb367bbeef9bbe008a3108
OtavioFSantos <otaviofs01@gmail.com>
2022-03-19 18:47:23 -0300

    login page prototype

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,22 @@
-export default function LoginPage(){
+import { useSession, signIn } from "next-auth/react"
+import { useEffect } from "react"
+import { useRouter } from 'next/router'
+
+export default function LoginPage() {
+    const { data: session } = useSession()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!!session && !!session.user) {
+            router.push('/')
+        }
+    }, [session])
+
+
     return (
         <article>
             <h1>Login</h1>
+            <button onClick={() => signIn("github")}>Login with Github</button>
         </article>
     )
 }
