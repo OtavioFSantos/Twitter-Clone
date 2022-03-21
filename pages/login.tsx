0eeec519348c849c7b56491fda1ae12434a92e58
OtavioFSantos <otaviofs01@gmail.com>
2022-03-21 10:27:22 -0300

    login page stylized

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import styles from "../styles/Login.module.css";
 
 export default function LoginPage() {
   const { data: session } = useSession();
@@ -13,9 +14,15 @@ export default function LoginPage() {
   }, [session]);
 
   return (
-    <article>
-      <h1>Login</h1>
-      <button onClick={() => signIn("github")}>Login with Github</button>
+    <article className={styles.login_page}>
+      <h1 className={styles.login_h1}>
+        Welcome!
+        <br />
+        Please login to continue
+      </h1>
+      <button className={styles.login_button} onClick={() => signIn("github")}>
+        Login with Github
+      </button>
     </article>
   );
 }
