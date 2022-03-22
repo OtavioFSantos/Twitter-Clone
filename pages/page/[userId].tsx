09f95a81ed218b10d5a2c26a1c66d9bd869284e6
OtavioFSantos <otaviofs01@gmail.com>
2022-03-22 08:49:03 -0300

    user page contains all basic info

--- pages/page/[userId].tsx
+++ pages/page/[userId].tsx
@@ -1,4 +1,9 @@
 import type { NextPageContext } from "next";
+import { TweetsService } from "../../lib/tweets/services/TweetsService";
+import { PrismaClient } from "@prisma/client";
+import styles from "../../styles/User.module.css";
+import { Tweet } from "../../lib/tweets/components/Tweet";
+import { useTimeline } from "../../lib/tweets/hooks/use-timeline";
 
 /**
  * Tarefas:
@@ -6,22 +11,50 @@ import type { NextPageContext } from "next";
  * - Mostrar a foto do usuário
  * - Mostrar os tweets do usuário ordenado de forma decrescente de acordo com a data de criação
  */
-export default function UserPage({ userId }) {
-  console.log({ userId });
+
+type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
+
+export default function UserPage(props: Props) {
+  const query = useTimeline(props.tweets);
+
   return (
-    <article>
-      <h1>Página do Otávio</h1>
-    </article>
+    <div>
+      <nav className={styles.navbar}>
+        <img
+          className={styles.user_image}
+          src={query.data[0].user.image}
+          alt="profile picture"
+        />
+        <a className={styles.user_name}>{query.data[0].user.name}</a>
+        <a className={styles.user_email}>{query.data[0].user.email}</a>
+        <a className={styles.user_tweets}>
+          {Object.keys(query.data).length} tweets
+        </a>
+      </nav>
+
+      <section className={styles.centralize}>
+        <div className={styles.timeline}>
+          {query.data.map((tweet) => (
+            <Tweet key={tweet.id} tweet={tweet} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
 
 export async function getServerSideProps(ctx: NextPageContext) {
   const userId = ctx.query.userId;
-  console.log({ userId });
+  const prisma = new PrismaClient();
+  const service = new TweetsService(prisma);
+  const userTweets = await service.listUserTweets(JSON.stringify(userId));
 
   return {
     props: {
-      userId,
+      tweets: userTweets.map((tweet) => ({
+        ...tweet,
+        createdAt: tweet.createdAt.toISOString(),
+      })),
     },
   };
 }
