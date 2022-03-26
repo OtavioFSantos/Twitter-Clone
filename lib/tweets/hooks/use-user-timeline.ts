3d66feb932985cd3010f05c15f639ad692235577
OtavioFSantos <otaviofs01@gmail.com>
2022-03-26 18:21:42 -0300

    fixed shared cache key

--- lib/tweets/hooks/use-user-timeline.ts
+++ lib/tweets/hooks/use-user-timeline.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 
 export const useUserTimeline = (userId: string) =>
-  useQuery("userId", () =>
+  useQuery(`userId/${userId}`, () =>
     fetch(`/api/tweets/${userId}`).then((res) => res.json())
   );
