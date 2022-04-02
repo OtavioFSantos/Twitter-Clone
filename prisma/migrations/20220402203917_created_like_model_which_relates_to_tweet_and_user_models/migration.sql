505ac2d4396082b8194fa054cdf45418725f286d
OtavioFSantos <otaviofs01@gmail.com>
2022-04-02 17:40:17 -0300

    add likes model

---
+++ prisma/migrations/20220402203917_created_like_model_which_relates_to_tweet_and_user_models/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `replies` on the `Tweet` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Tweet" DROP COLUMN "replies";
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "tweetId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_tweetId_fkey" FOREIGN KEY ("tweetId") REFERENCES "Tweet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
