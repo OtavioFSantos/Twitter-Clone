11edbc19ec8c459bbf844cc15e6711fecaac53a8
OtavioFSantos <otaviofs01@gmail.com>
2022-04-16 14:56:52 -0300

    sign in button only appear when not logged

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { useTimeline } from "../lib/tweets/hooks/use-timeline";
 import { TweetsService } from "../lib/tweets/services/TweetsService";
 import styles from "../styles/Index.module.css";
 import { db } from "../prisma/db";
+import { useSession } from "next-auth/react";
 
 type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
 
@@ -12,13 +13,22 @@ const service = new TweetsService(db);
 
 export default function IndexPage(props: Props) {
   const query = useTimeline(props.tweets);
+  const { data: session } = useSession();
 
   return (
     <main>
       <nav className={styles.navbar}>
-        <Link href="/login">
-          <a className={styles.signin}>Sign in</a>
-        </Link>
+        {!!session ? (
+          <img
+            className={styles.user_image}
+            src={session.user.image}
+            alt="profile picture"
+          />
+        ) : (
+          <Link href="/login">
+            <a className={styles.signin}>Sign in</a>
+          </Link>
+        )}
       </nav>
       <article className={styles.centralize}>
         <section className={styles.new_tweet}>
