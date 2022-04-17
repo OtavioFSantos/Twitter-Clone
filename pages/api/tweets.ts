9b0f9fe401c469673168302852453bce52d4aee2
OtavioFSantos <otaviofs01@gmail.com>
2022-04-16 22:32:07 -0300

    public api's

---
+++ pages/api/tweets.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { TweetsService } from "../../lib/tweets/services/TweetsService";
+import { db } from "../../prisma/db";
+
+const tweetsService = new TweetsService(db);
+
+/**
+ * Returns a list of tweets using the filter
+ *  userId?: string
+ */
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const userId = req.query.userId;
+
+  if (userId) {
+    // busca os tweets do usuário
+    const userTweets = await tweetsService.listByUserId(userId as string);
+    return res.status(200).json(userTweets);
+  }
+
+  // busca os tweets de todo mundo
+  const allTweets = await tweetsService.list();
+  return res.status(200).json(allTweets);
+}
