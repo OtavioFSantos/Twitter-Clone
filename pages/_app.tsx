44a9946825c8aac226f9868fce75737fb7f04993
OtavioFSantos <otaviofs01@gmail.com>
2022-04-16 22:29:04 -0300

    ReactQueryDevTools added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </SessionProvider>
   );
