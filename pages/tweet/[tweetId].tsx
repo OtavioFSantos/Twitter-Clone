d8960bb149f37426c7bcd72a6d691c6fac97a543
OtavioFSantos <otaviofs01@gmail.com>
2022-04-17 12:07:57 -0300

    added title on page and form only appear when logged

--- pages/tweet/[tweetId].tsx
+++ pages/tweet/[tweetId].tsx
@@ -5,7 +5,10 @@ import { db } from "../../prisma/db";
 import { useRepliesTimeline } from "../../lib/tweets/hooks/use-replies";
 import type { NextPageContext } from "next";
 import styles from "../../styles/Tweet.module.css";
-import Link from "next/link";
+import { Navbar } from "../../lib/shared/components/Navbar";
+import { useSession } from "next-auth/react";
+import { Fragment } from "react";
+import Head from "next/head";
 
 const tweetService = new TweetsService(db);
 
@@ -13,20 +16,31 @@ type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
 
 export default function TweetPage(props: Props) {
   const query = useRepliesTimeline(props.tweet.id);
+  const { data: session } = useSession();
 
   return (
     <article>
-      <nav className={styles.navbar}>
-        <Link href="../">
-          <a className={styles.home}>Home</a>
-        </Link>
-      </nav>
+      <Head>
+        <title>
+          {props.tweet.user.name} on Twitter: "{props.tweet.content}"
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="icon"
+          href="https://img.icons8.com/color/344/twitter--v1.png"
+        ></link>
+      </Head>
+      <Navbar />
       <section className={styles.centralize}>
         <div className={styles.timeline}>
           <Tweet key={props.tweet.id} tweet={props.tweet} />
         </div>
         <section className={styles.new_tweet}>
-          <CreateReplyForm key={props.tweet.id} replyId={props.tweet.id} />
+          {!!session ? (
+            <CreateReplyForm key={props.tweet.id} replyId={props.tweet.id} />
+          ) : (
+            <Fragment></Fragment>
+          )}
         </section>
         <span>
           <br />-<br />
