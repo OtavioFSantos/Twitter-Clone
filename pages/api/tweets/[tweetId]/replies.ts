1c1c31ea9a2ea2085c65330f852e71901c6ed68a
OtavioFSantos <otaviofs01@gmail.com>
2022-04-16 22:31:11 -0300

    replies.ts has been moved

---
+++ pages/api/tweets/[tweetId]/replies.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { TweetsService } from "../../../../lib/tweets/services/TweetsService";
+import { db } from "../../../../prisma/db";
+
+const tweetsService = new TweetsService(db);
+
+export default async function handleTweetReplies(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const tweetId = req.query.tweetId as string;
+  const replies = await tweetsService.listByReplyId(tweetId);
+
+  res.status(200).json(replies);
+}
