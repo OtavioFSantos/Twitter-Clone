86e36890cb17298ae3adccd735c91f5db2a46d60
OtavioFSantos <otaviofs01@gmail.com>
2022-04-25 22:45:45 -0300

    add endpoint for creating follower

---
+++ pages/api/user/[userId]/follow.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { UserService } from "../../../../lib/users/services/UserService";
+import { getSession } from "next-auth/react";
+import { db } from "../../../../prisma/db";
+
+const userService = new UserService(db);
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const userId = req.query.userId as string;
+  const session = await getSession({ req });
+
+  const currentUser = await userService.findByEmail(session.user.email);
+  let currentUserAsFollower = await userService.findUserAsFollowerById(
+    currentUser.id
+  );
+
+  if (!currentUserAsFollower) {
+    currentUserAsFollower = await userService.createFollower(currentUser.id);
+  }
+
+  await userService.updateRelation(userId, currentUserAsFollower.id);
+  return res.status(201).json({});
+}
