6cd3afba6020ec4ff0c2f4feffce7395a5421142
OtavioFSantos <otaviofs01@gmail.com>
2022-04-25 22:44:02 -0300

    created follower services

--- lib/users/services/UserService.ts
+++ lib/users/services/UserService.ts
@@ -12,6 +12,85 @@ export class UserService {
   findById(id: string) {
     return this.db.user.findUnique({
       where: { id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        image: true,
+        followers: {
+          select: {
+            followerId: true,
+          },
+        },
+      },
+    });
+  }
+
+  findUserAsFollowerById(id: string) {
+    return this.db.follower.findUnique({
+      where: { id },
+    });
+  }
+
+  listFollowers(userId: string) {
+    return this.db.followersOnUser.findMany({
+      where: {
+        userId: userId,
+      },
+      select: {
+        follower: {
+          select: {
+            user: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  createFollower(id: string) {
+    return this.db.follower.create({
+      data: {
+        userId: id,
+      },
+    });
+  }
+
+  deleteFollower(userId: string, followerId: string) {
+    return this.db.followersOnUser.delete({
+      where: {
+        userId_followerId: {
+          userId,
+          followerId,
+        },
+      },
+    });
+  }
+
+  updateRelation(userId: string, followerId: string) {
+    return this.db.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        followers: {
+          connectOrCreate: {
+            where: {
+              userId_followerId: {
+                followerId,
+                userId,
+              },
+            },
+            create: {
+              followerId,
+            },
+          },
+        },
+      },
     });
   }
 }
